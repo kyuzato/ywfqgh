1f0b5eeb7ba8b8c414e6d635fa9f3d8c6819769e
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-05-01 09:14:54 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,4 +1,4 @@
 cp CREDS/config.ini REPOS/config.ini
 cd REPOS
 docker build . -t catarina
-docker run -d --restart on-failure --name CatarinaRobot catarina
+docker run --name CatarinaRobot catarina
